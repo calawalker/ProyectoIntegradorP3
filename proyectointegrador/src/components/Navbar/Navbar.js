a062d5a941d9fe4a1855faec94b709334b5899e5
Cala Walker <you@example.com>
2022-09-19 18:39:19 -0300

    correcciones en navbar

--- proyectointegrador/src/components/Navbar/Navbar.js
+++ proyectointegrador/src/components/Navbar/Navbar.js
@@ -10,71 +10,28 @@ import Notfound from "../../screens/Notfound/Notfound"
 import Detallealbum from "../../screens/Detallealbum/Detallealbum"
 import Detalletracks from "../../screens/Detalletracks/Detalletracks"
 
-class Navbar extends Component {
-
-    constructor(props) {
-        super(props)
-        this.state = {
-            personajes: [],
-            backup: [],
-            prueba: ''
-        }
-    }
-
-    componentDidMount() {
-        fetch('https://thingproxy.freeboard.io/fetch/https://api.deezer.com/chart/0/albums')
-            .then(resp => resp.json())
-            .then(data => this.setState({
-                personajes: data.results,
-                backup: data.results
-            }))
-            .catch(err => console.log(err))
-    }
-
-    componentDidUpdate() {
-    }
-
-   
-
-    /*     filtrarAlbumes(title){
-            let arrayFiltrado = 
-            this.state.backup.filter
-            (album => album.title.toLowerCase().includes(title.toLowerCase()))
-            this.setState({
-                albums: arrayFiltrado
-            })
-        } */
-
-    render() {
-        return (
-            <>
-                <nav>
-                    <ul className="main-nav">
-                        <li><Link to="/">Home</Link></li>
-                        <li><Link to="/favoritos">Favoritos</Link></li>
-                        <li><Link to="/todoslosalbumes">Todos los albumes</Link></li>
-                        <li><Link to="/todoslostracks">Todos los tracks</Link></li>
-                    </ul>
-                    
-                    <ul className="user">
-                        <li>{this.props.nombre}
-                            <img src="./img/logo.png" alt="logo" />
-                        </li>
-                    </ul>
-                </nav>
-                <Switch>
-                    <Route path='/' exact={true} component={Home} />
-                    <Route path='/favoritos' component={Favoritos} />
-                    <Route path='/todoslosalbumes' component={Vertodoslosalbumes} />
-                    <Route path='/todoslostracks' component={Vertodoslostracks} />
-                    <Route path='/detallealbum/:id' component={Detallealbum} ></Route>
-                    <Route path='/detalletracks/:id' component={Detalletracks} ></Route>
-                    <Route path='' component={Notfound} />
-                </Switch>
-            </>
-        )
-    }
-}
-
+function Navbar() {  
+            return (
+                <>
+                    <nav>
+                        <ul className="main-nav">
+                            <li><Link to="/">Home</Link></li>
+                            <li><Link to="/favoritos">Favoritos</Link></li>
+                            <li><Link to="/todoslosalbumes">Todos los albumes</Link></li>
+                            <li><Link to="/todoslostracks">Todos los tracks</Link></li>
+                        </ul>
+                    </nav>
+                    <Switch>
+                        <Route path='/' exact={true} component={Home} />
+                        <Route path='/favoritos' component={Favoritos} />
+                        <Route path='/todoslosalbumes' component={Vertodoslosalbumes} />
+                        <Route path='/todoslostracks' component={Vertodoslostracks} />
+                        <Route path='/detallealbum/:id' component={Detallealbum} ></Route> 
+                        <Route path='/detalletracks/:id' component={Detalletracks} ></Route>
+                        <Route path='' component={Notfound} />
+                    </Switch>
+                </>
+            )
+  }
 
 export default Navbar
